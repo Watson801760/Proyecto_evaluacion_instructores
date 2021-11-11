e6c387637019cc7d235746bf72ea51e64f2167f9
Watson801760 <waccdulce@gmail.com>
2021-11-10 20:29:24 -0500

    error 405

--- src/java/ModeloDAO/AprendizDAO.java
+++ src/java/ModeloDAO/AprendizDAO.java
@@ -8,6 +8,9 @@ package ModeloDAO;
 import ModeloVO.AprendizVO;
 import Util.Conexion;
 import Util.Crud;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,6 +18,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -224,5 +231,55 @@ public class AprendizDAO extends Conexion implements Crud{
         }*/
         return ListaAprendiz;
     }
+     
+  public boolean Subir_Archivo (String archivo12) throws FileNotFoundException, IOException{
+      FileInputStream archivo = new FileInputStream(archivo12);
+        System.out.println("1"+archivo);
+        XSSFWorkbook libro = new XSSFWorkbook(archivo);
+        System.out.println("2");
+        XSSFSheet hoja =  libro.getSheetAt(0);
+        System.out.println("3");
+        int numero_Filas = hoja.getLastRowNum();
+        System.out.println("4");
+        for(int i = 0;  i<= numero_Filas ; i++){
+            try {
+            Row fila = hoja.getRow(i);
+            Conexion conexion = new Conexion();
+            Connection conectar = conexion.obtenerConexion();
+            PreparedStatement insertar = conectar.prepareStatement("insert into aprendiz (`nombre`,`apellido`,`correo`,`numIdentidad`,`tipoDocumento`,`idUsuario(FK)`) values(?,?,?,?,?,?)");
+            System.out.println("5"+ 1);
+            insertar.setString(1,fila.getCell(0).getStringCellValue());
+            System.out.println("6"+ 2);
+            insertar.setString(2,fila.getCell(1).getStringCellValue());
+            System.out.println("7"+ 3);
+            insertar.setString(3,fila.getCell(2).getStringCellValue());
+            System.out.println("NEAAAAA: "+(int) fila.getCell(3).getNumericCellValue());
+            insertar.setInt(4, (int) fila.getCell(3).getNumericCellValue());
+            System.out.println("9"+ 5);
+            insertar.setString(5,fila.getCell(4).getStringCellValue());
+            System.out.println("10"+ (int) fila.getCell(5).getNumericCellValue());
+            insertar.setInt(6, (int) fila.getCell(5).getNumericCellValue());
+            insertar.executeUpdate();
+            } catch (Exception e) {
+            Logger.getLogger(AprendizDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        }
+        System.out.println("5");
+     
+       
+            
+            
+         
+        /*finally{    
+            try {
+                this.cerrarConexion();
+                
+            } catch (SQLException e) {
+                Logger.getLogger(PreguntasDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }*/
+        return operacion;
+    }
+       
 }
 
